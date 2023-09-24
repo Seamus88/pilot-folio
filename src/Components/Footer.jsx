35175a4eacb1e6d70233ea47aa8c1e52d554e201
James Cosgrove <Seamus.Coz@gmail.com>
2023-09-23 19:19:35 -0500

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -103,7 +103,7 @@ const Footer = (props) => {
         )}
       </div>
       <p className="small" style={{ marginTop: 0, color: "white" }}>
-        Created by {name}
+        Created by James Cosgrove
       </p>
     </div>
   );
